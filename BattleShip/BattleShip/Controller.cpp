6fe6c6f139535eb9c4ae2afb3a27d282e56d9123
Yulia Lisova <yulia.s.lisova@gmail.com>
2017-08-06 15:21:50 +0300

    IsItPossible method controller.cpp - fixed

--- BattleShip/BattleShip/Controller.cpp
+++ BattleShip/BattleShip/Controller.cpp
@@ -139,135 +139,40 @@ using namespace std;
 }
 
 
-bool  ship_is_good
-    (
-        int         size, 
-        bool        is_horiz, 
-        int         row_top, 
-        int         col_left, 
-        const Field  field
-    )
+
+
+
+
+	bool Controller:: IsItPossible (const Field& field, const int & deckNumbers, const int & orientation, const  int & x, const int & y) const
 {
-	const int  FIELD_DIM   = 10;
-
-    if(is_horiz)
-    {
-        for(int i =   std::max(0,              row_top - 1); 
-                i <=  std::min( - 1,  row_top + 1); 
-                ++i)
-        {
-            for(int j =   std::max(0,              col_left - 1); 
-                    j <=  std::min(FIELD_DIM - 1,  col_left + size); 
-                    ++j)
-            {
-				if(field.array[i][j].cellPosition_ != EMPTY ) return false;
-            }        
-        }
-        return  true;
-    }
-    else//вертикальный
-    {
-        for(int i =   std::max(0,             row_top - 1); 
-                i <=  std::min(FIELD_DIM - 1, row_top + size); 
-                ++i)
-        {
-            for(int j =   std::max(0,             col_left - 1); 
-                    j <=  std::min(FIELD_DIM - 1, col_left + 1); 
-                    ++j)
-            {
-                if(field.array[i][j].cellPosition_ != EMPTY ) return false;
-            }        
-        }
-        return  true;
-    }
-}
-
-
-void  set_ship_with_size(Ship* ship, Field * field)
-{
-	const int  FIELD_DIM   = 10;
-	int size = ship->size_;
-
-    bool  is_horiz  = rand() % 2 == 0;
-    int   row_top   = 0;
-    int   col_left  = 0;
- 
-    do
-    {
-        do
-        {
-            row_top = rand() % FIELD_DIM;
-        }while(   !is_horiz 
-               && row_top > FIELD_DIM - size);
- 
-        do
-        {
-            col_left = rand() % FIELD_DIM;
-        }while(   is_horiz 
-               && col_left > FIELD_DIM - size);        
-    }while(!ship_is_good(size, is_horiz, row_top, col_left,  * field)); 
- 
-    if(is_horiz)
-    {
-        for(int j = col_left; j < col_left + size; ++j)
-        {
-			field->array[row_top][j].cellPosition_ = DECK;
-
-			ship->decks_.push_back(&field->array[row_top][j]);
-				field->array[row_top][j].AddObserver(ship);
-        }
-    }
-    else//вертикальный
-    {
-        for(int i = row_top; i < row_top + size; ++i)
-        {
-			field->array[i][col_left].cellPosition_= DECK;
-			ship->decks_.push_back(&field->array[i][col_left]);
-				field->array[i][col_left].AddObserver(ship);
-        }    
-    }
-}
-
-	bool Controller:: IsItPossible (const Field& field, const int & deckNumbers,const int & orientation, const  int & y, const int & x) const
-{
-	if (field.array[y][x].cellPosition_ != EMPTY) return false;
 	switch (orientation)
-	{
-	  case 1:
-		  if (x + deckNumbers > 9) return false;
-		  else 
-		  {
-		  
-			   bool returning = true;
-			  for (int i = x+1; i <= deckNumbers; i ++)
-		      {
-			  if (field.array[i][x].cellPosition_ != EMPTY) returning =  false;
-			 // break;
-		      }
-             return returning;
-		  }
+		{
+		case 1://horizontal
+			{if (y + deckNumbers > 10) return false;
+			else 
+			{
+				for (int i = y, iteration = 0; iteration < deckNumbers; i ++, iteration ++)
+				{if (field.array[x][i].cellPosition_ != EMPTY) return false;}			
+			}
+			}
+			break;
+
+		case 2:
+			{
+			if (x + deckNumbers > 10) return false;
+			else 
+			{
+				for (int i = x, iteration = 0; iteration < deckNumbers; i ++, iteration ++)
+				{
+					if (field.array[i][y].cellPosition_ != EMPTY) return false;
+
+				}			
+			}
+			}
+			break;
+		}
+
 	
-		
-		  break;
-	  case 2:
-		  if (y + deckNumbers > 9) return false;
-
-
-		
-		  else 
-		  
-		  {
-			  bool returning = true;
-			  for (int i = y+1; i <= deckNumbers; i ++)
-		      {
-			  if (field.array[i][x].cellPosition_ != EMPTY) returning =  false;
-			 // break;
-		      }
-             return returning;
-		  }
-
-		  break;
-	}
 
 	return true;
 }
